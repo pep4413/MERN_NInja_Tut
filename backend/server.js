e1a3aef5cd312f4bd3e1bf1beff61bbbd8a1efdc
Pat P <patrickeperkins@gmail.com>
2023-06-05 19:03:48 -0400

    lesson 4 commit

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 require('dotenv').config()
 const express = require('express')
+const mongoose = require('mongoose')
 const workoutRoutes = require('./routes/workouts')
 
 // express app
@@ -16,7 +17,13 @@ app.use((req, res, next) => {
 // route handler
 app.use('/api/workouts', workoutRoutes)
 
-// listen for requests
-app.listen(process.env.PORT, () => {
-    console.log('listening on port 3000')
-})
+// connect to DB
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => {
+        // listen for requests
+        app.listen(process.env.PORT, () => {
+            console.log('listening on port', process.env.PORT)
+        })
+    })
+    .catch((err) => console.log(err))
+
