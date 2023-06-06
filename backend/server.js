be9db03b5b70b7601139be7094f7f707cd79567b
Pat P <patrickeperkins@gmail.com>
2023-06-06 10:32:29 -0400

    lesson 9 commit

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 require('dotenv').config()
 const express = require('express')
 const mongoose = require('mongoose')
+const cors = require('cors')
 const workoutRoutes = require('./routes/workouts')
 
 // express app
@@ -8,6 +9,7 @@ const app = express()
 
 //middleware
 app.use(express.json())
+app.use(cors())
 
 app.use((req, res, next) => {
     console.log(req.path, req.method)
@@ -26,4 +28,3 @@ mongoose.connect(process.env.MONGO_URI)
         })
     })
     .catch((err) => console.log(err))
-
---
+++ frontend/src/pages/Home.js
@@ -0,0 +1,30 @@
+import { useEffect, useState } from "react";
+import WorkoutDetails from "../components/workoutDetails";
+
+const Home = () => {
+    const [workouts, setWorkouts] = useState(null)
+
+    useEffect(() => {
+        const fetchWorkouts = async () => {
+            const response = await fetch('http://localhost:4000/api/workouts')
+            const json = await response.json()
+
+            if (response.ok) {
+                setWorkouts(json)
+            }}
+        
+        fetchWorkouts()
+    }, [])
+
+    return ( 
+        <div className="home">
+            <div className="workouts">
+                {workouts && workouts.map((workout) => (
+                    <WorkoutDetails key={workout._id} workout={workout} />
+                ))}
+            </div>
+        </div>
+     );
+}
+ 
+export default Home;
